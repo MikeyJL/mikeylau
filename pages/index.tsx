d7add2e16c043efafed67cb6c10243fe53d7331f
Mikey Lau <mikey@3sidedcube.com>
2023-03-26 00:42:40 +0000

    refactor: Add more achievements

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
+import { PageWrapper } from "@/components";
+import { ExternalLink } from "@/components/svgs";
 import Head from "next/head";
-import { PageWrapper } from "../components";
 
 type Achievement = {
   name: string;
-  description?: string;
+  description: string;
   date: string;
+  url?: string;
 };
 
 const Home = () => {
@@ -16,7 +18,21 @@ const Home = () => {
     },
     {
       name: "CFO-CF02 Snowflake",
+      description: "Passed",
       date: "March 23",
+      url: "https://www.credly.com/badges/eb55e937-7518-4a94-941f-9b51fd579514/public_url",
+    },
+    {
+      name: "DAMA CDMP Associate",
+      description: "Pending",
+      date: "July 23",
+      url: "https://www.dama.org/cpages/cdmp-information",
+    },
+    {
+      name: "IAPP CIPP/E",
+      description: "Pending",
+      date: "December 23",
+      url: "https://iapp.org/certify/cippe",
     },
   ];
 
@@ -26,13 +42,26 @@ const Home = () => {
         <title>Mikey Lau</title>
       </Head>
 
-      <main className="flex flex-1 flex-col">
+      <main className="z-10 flex flex-1 flex-col">
         <div className="mt-auto ml-auto flex w-fit flex-col">
           {/* Achievements */}
           {achievements.map((item) => (
             <div key={item.name} className="mt-4">
-              <p className="font-semibold">{item.name}</p>
+              {/* Heading */}
+              {item.url ? (
+                <a
+                  href={item.url}
+                  target="_blank"
+                  className="flex items-center transition-opacity hover:opacity-60"
+                >
+                  <p className="font-semibold">{item.name}</p>
+                  <ExternalLink className="ml-2 h-4 w-4" />
+                </a>
+              ) : (
+                <p className="font-semibold">{item.name}</p>
+              )}
 
+              {/* Description */}
               <div className="flex items-center text-sm">
                 {item.description && <p>{item.description} |&nbsp;</p>}
                 <p>{item.date}</p>
